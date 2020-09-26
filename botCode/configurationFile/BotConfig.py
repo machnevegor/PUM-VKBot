84139f25acc7fba9ed5710559cc23008ea8bf627
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-09-26 20:11:51 +0300

    Update BotConfig.py

--- botCode/configurationFile/BotConfig.py
+++ botCode/configurationFile/BotConfig.py
@@ -1,7 +1,7 @@
 # Authors of the project:
 # 1-MachnevEgor_https://vk.com/machnev_egor
 # 2-SchalimovDmitriy_https://vk.com/astronaut_without_spaceship
-# 3-ArsenyKarimov_https://vk.com/@id222338543
+# 3-ArsenyKarimov_https://vk.com/id222338543
 # 4-MihailMarkov_https://vk.com/mixxxxail
 # Contacts in email:
 # 1-meb.official.com@gmail.com
@@ -14,7 +14,7 @@ BotToken = ""  # bot token
 # Authors of the project:
 # 1-MachnevEgor_https://vk.com/machnev_egor
 # 2-SchalimovDmitriy_https://vk.com/astronaut_without_spaceship
-# 3-ArsenyKarimov_https://vk.com/@id222338543
+# 3-ArsenyKarimov_https://vk.com/id222338543
 # 4-MihailMarkov_https://vk.com/mixxxxail
 # Contacts in email:
 # 1-meb.official.com@gmail.com
