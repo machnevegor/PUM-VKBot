0efba22969079702fc7ca06ca23b3e95abf589fe
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-10-04 18:43:17 +0300

    Add files via upload

---
+++ botCode/maintenanceService/startMaintenanceService.bat
@@ -0,0 +1,24 @@
+@: Authors of the project:
+@: 1-MachnevEgor_https://vk.com/machnev_egor
+@: 2-SchalimovDmitriy_https://vk.com/astronaut_without_spaceship
+@: 3-ArsenyKarimov_https://vk.com/id222338543
+@: 4-MihailMarkov_https://vk.com/mixxxxail
+@: Contacts in email:
+@: 1-meb.official.com@gmail.com
+@: 2-dmitriy-shalimov@yandex.ru
+@: 3-arseny.karimov@gmail.com
+@: 4-mihailmarkov2004@gmail.com
+
+python MainBotLogic.py --user
+pause
+
+@: Authors of the project:
+@: 1-MachnevEgor_https://vk.com/machnev_egor
+@: 2-SchalimovDmitriy_https://vk.com/astronaut_without_spaceship
+@: 3-ArsenyKarimov_https://vk.com/id222338543
+@: 4-MihailMarkov_https://vk.com/mixxxxail
+@: Contacts in email:
+@: 1-meb.official.com@gmail.com
+@: 2-dmitriy-shalimov@yandex.ru
+@: 3-arseny.karimov@gmail.com
+@: 4-mihailmarkov2004@gmail.com
