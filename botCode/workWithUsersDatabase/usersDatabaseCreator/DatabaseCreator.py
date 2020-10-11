aef38d5381ad2d91f008ffb871f170640dec8c25
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-10-11 22:56:22 +0300

    Да, дада:/
    Новый update

--- botCode/workWithUsersDatabase/usersDatabaseCreator/DatabaseCreator.py
+++ botCode/workWithUsersDatabase/usersDatabaseCreator/DatabaseCreator.py
@@ -95,8 +95,8 @@ def database_interpreter(excel_source, sheet_name, columns, source_for_user, col
 
 
 # start work - make sure everything is correct
-database_interpreter(excel_source="SomeExcelWithNewUsers.xlsx", sheet_name="Лист1", columns=["A", "B", "C"],
-                     source_for_user="workWithExcelFile/excelDatabase/10class/10class.xlsx",
+database_interpreter(excel_source="usersExcelDatabase/###xlsx", sheet_name="Лист1",
+                     columns=["#", "#", "#"], source_for_user="##class/##class.xlsx",
                      columns_for_user="['A', 'B', 'E']", extra_cells=1, txt_source="UsersDatabase.txt")
 
 # Authors of the project:
