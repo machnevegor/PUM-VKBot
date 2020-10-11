554ebfa9c1a9b0ee78692205c3bd9bfb4a2a7eaf
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-10-12 00:22:12 +0300

    Add files via upload

--- botCode/workWithUsersDatabase/UserSearcher.py
+++ botCode/workWithUsersDatabase/UserSearcher.py
@@ -111,6 +111,11 @@ def searching_user_in_database(database_source, user_id):
             print("!!! ERROR: Users database - BROKEN !!!")
             print(f"Reason: {E}")
 
+
+# adding_user_in_database(database_source="UsersDatabase.txt", full_name="# # #", user_id="id#",
+#                         source_for_user="excelDatabase/##class/##class.xlsx", sheet_name="#, #",
+#                         columns_for_user=["#", "#", "#"], extra_cells=1)
+
 # Authors of the project:
 # 1-MachnevEgor_https://vk.com/machnev_egor
 # 2-SchalimovDmitriy_https://vk.com/astronaut_without_spaceship
@@ -121,7 +126,3 @@ def searching_user_in_database(database_source, user_id):
 # 2-dmitriy-shalimov@yandex.ru
 # 3-arseny.karimov@gmail.com
 # 4-mihailmarkov2004@gmail.com
-
-# adding_user_in_database(database_source="UsersDatabase.txt", full_name="# # #", user_id="id#",
-#                         source_for_user="excelDatabase/10class/10class.xlsx", sheet_name="#, #",
-#                         columns_for_user=["#", "#", "#"], extra_cells=1)
