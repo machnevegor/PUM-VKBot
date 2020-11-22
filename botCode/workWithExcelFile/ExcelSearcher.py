05fa2c43c1ad64fdf97f570a9a518844d3c3f3f5
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-11-22 11:05:19 +0300

    Add files via upload

--- botCode/workWithExcelFile/ExcelSearcher.py
+++ botCode/workWithExcelFile/ExcelSearcher.py
@@ -118,7 +118,7 @@ def selective_data_search(excel_source, columns, extra_cells, sheet_name, start_
                         output_day_schedule.append(f"👉Приходить ко {quantity_transfers + 1} уроку👈")
                 if additional_information_output_data_array[quantity_transfers] != "None":
                     output_day_schedule.append(
-                        f"🧠 {additional_information_output_data_array[quantity_transfers]}({teachers_output_data_array[quantity_transfers]} & {cabinets_output_data_array[quantity_transfers]})")
+                        f"{quantity_transfers + 1}. 🧠{additional_information_output_data_array[quantity_transfers]}({teachers_output_data_array[quantity_transfers]} & {cabinets_output_data_array[quantity_transfers]})")
                 else:
                     output_day_schedule.append(
                         f"{quantity_transfers + 1}. {lessons_output_data_array[quantity_transfers]}({teachers_output_data_array[quantity_transfers]} & {cabinets_output_data_array[quantity_transfers]})")
