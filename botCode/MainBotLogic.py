0761885ce7b10f2f561fa07ec7d87508587ff6d7
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-12-01 21:31:28 +0300

    Add files via upload

--- botCode/MainBotLogic.py
+++ botCode/MainBotLogic.py
@@ -23,7 +23,7 @@ from workWithUsersDatabase import UserSearcher
 from workWithExcelFile import ExcelSearcher as ExcelSearcher
 from specialScripts import CompilationNews as CompilationNews
 
-# full error log output(without auto-reconnection)
+# full error log output (without auto-reconnection)
 error_checking_switch = False
 # time to restart the bot
 reboot_time = 5
