fe2df6f52c0d8f67575a316572b69ca5a5358192
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-12-25 09:08:12 +0300

    Add files via upload

--- botCode/MainBotLogic.py
+++ botCode/MainBotLogic.py
@@ -25,7 +25,7 @@ from workWithExcelFile import ExcelSearcher as ExcelSearcher
 from specialScripts import CompilationNews as CompilationNews
 
 # full error log output (without auto-reconnection)
-error_checking_switch = True
+error_checking_switch = False
 # time to restart the bot
 reboot_time = 5
 
